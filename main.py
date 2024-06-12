9ac5a195715b33ff8d1d4c01443beb1ddc3fd1e6
Mud Man <chickensrule111@gmail.com>
2024-06-12 13:35:33 +1000

    yayayayay

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from random import randint
 from time import sleep
+GOBBYLIST = ['Grizzletongue', 'Grumbletoad', 'Rumblebelly', 'Rumblestomp', 'Noggleblight', 'Goblintide', 'Glimmergob', 'Wretchwhisker', 'Fangsnarl', 'Snagglewort', 'Sludgegrip', 'Snickersnatch', 'Gloomclaw', 'Sliverfang', 'Wartnose', 'Muckspindle', 'Thistleknob', 'Moldfinger', 'Gnarltooth', 'Slimefoot', 'Grumblegrim', 'Snickergrin', 'Fizzlefingers', 'Bogbelch', 'Slurgtail', 'Sludgeclaw', 'Miregaze', 'Grumblegut', 'Wretchling', 'Nibblesnatch', 'Slithergrub', 'Snaggleclaw', 'Sludgefist', 'Slurpblight', 'Grizzleknob', 'Wartbelly', 'Sludgejaw', 'Nogglegrin', 'Moldywhisk', 'Rumblemuck', 'Fazefoot', 'Glimmerwart', 'Snaktongue', 'Gnarlbelly', 'Thistlefang', 'Wobblewart', 'Snikernose', 'Goblintooth', 'Nettlegrin', 'Gloomshadow', 'Sludgejaw', 'Snickerwart', 'Rumblefoot', 'Gobsmudge', 'Sludgebelle', 'Golintid', 'Nogglebelly', 'Mosswhisk', 'Grizzletooth', 'Wartnibble', 'Wretchfang', 'Slitherclaw', 'Grumbleclaw', 'Grumblethorn', 'Wobblethumb', 'Snaggletoad', 'Thistlenose', 'Sludgebelch', 'Fizzlefoot', 'Sliverclaw', 'Nibblesnarl', 'Glimmerbelly', 'Snagglenose', 'Slurpgrub', 'Slimegob', 'Wretchwhisk', 'Thistlejaw', 'Snickersnarl', 'Sludgepaw', 'Gobblefist', 'Grizzlegrin', 'Bogbelch', 'Slitherjaw', 'Slurpnose', 'Gnarlclaw', 'Wartbelly', 'Thistletide', 'Slurptongue', 'Moldywhisk', 'Gontongue', 'Glimmergrim', 'Snickergaze', 'Grumblefoot', 'Fizzlegob', 'Rumblenose', 'Grumblegaze'] 
 def text(toprint):
-    texttime = 0.75/len(toprint)
+    texttime = 0.03
     toprint = str(toprint)
     for char in toprint:
         print(char,flush=True,end='')
@@ -12,15 +13,16 @@ def text(toprint):
     print('\n',end='')
 class Goblin():
     def __init__(self):
-        pos = [randint(-3,3),randint(-3,3)]
-        health = randint(1,3)
+        self.pos = [randint(-3,3),randint(-3,3)]
+        self.health = randint(1,3)
         self.damage = randint(1,2)
+        self.name = GOBBYLIST[randint(0,len(GOBBYLIST))]
     def turn(self):
         if self.pos == Player.pos:
             self.battle()
     def battle(self):
         Player.health -= self.damage
-        text(f'Hit! You take {self.damage} damage from {self}')
+        text(f'Hit! You take {self.damage} damage from {self.name} The Goblin')
 class Player():
     pos = [0,0]
     health = 10
@@ -32,7 +34,8 @@ class Item():
     def __init__(self,name,x,y):
         self.pos = [x,y]
         self.name = name
-        
+
+
 Player1 = Player()
 Gobby = Goblin()
 text(Gobby.pos)
